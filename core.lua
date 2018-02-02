45662dc9c8fae59718d4acdb11dd2200f605a1b5
ReimuJF <slavalang@gmail.com>
2018-02-03 02:53:47 +0500

    funcimproovement

--- core.lua
+++ core.lua
@@ -1,11 +1,42 @@
-local function TakeScreenShot()
-	Screenshot()
-end
- 
-local af = CreateFrame("Frame")
-af:SetScript("OnEvent", TakeScreenShot)
-af:RegisterEvent("CHALLENGE_MODE_COMPLETED")
+local ae = 1;
 
---local f = CreateFrame("Frame")
---f:SetScript("OnEvent", TakeScreenShot)
---f:RegisterEvent("ACHIEVEMENT_EARNED")
+
+local function mpscrSlashCmdList(msg, editbox)
+   if ( msg == "enable" ) then
+      ae = 1;
+      print("mpscr enabled")
+      
+   elseif( msg == "disable") then
+      ae = 0;
+      print("mpscr disabled")    
+   elseif ( msg == "help") then
+      print("use /enable or /disable argument")
+   else
+      if ae==1 then 
+         print("mpscr ON")
+      else
+         print("mpscr OFF")
+      end
+   end
+end
+
+SLASH_MPSCR1 ='/mpscr'
+
+SlashCmdList["MPSCR"] = mpscrSlashCmdList
+
+local function TakeScreenShot()
+   if ae == 1 then
+      Screenshot()
+   else
+      print(":)")
+   end 
+end
+
+local function delayTakeScreenshot()
+   --print("test")
+   C_Timer.After(3, function() TakeScreenShot() end)
+end
+
+local af = CreateFrame("Frame")
+af:RegisterEvent("BANKFRAME_OPENED")
+af:SetScript("OnEvent", delayTakeScreenshot)
